6f00e571f16315387b735ca2dfe8320f4caac6a4
Faruk Akgul <faakgul@gmail.com>
2016-08-19 11:22:30 +1000

    update tinder.app settings

--- aiotinder/settings.py
+++ aiotinder/settings.py
@@ -2,11 +2,12 @@
 
 
 HEADERS = {
-    "User-Agent": "Tinder/5.3.2 (iPhone; iOS 9.3.3; Scale/2.00)",
+    "User-Agent": "Tinder/5.4.0 (iPhone; iOS 9.3.4; Scale/2.00)",
     "Content-Type": "application/json",
-    "os_version": "90000300003",
+    "os_version": "90000300004",
     "platform": "ios",
-    "app-version": "1637"
+    "app-version": "1734",
+    "x-client-version": "54047"
 }
 
 API_URL = "https://api.gotinder.com/"

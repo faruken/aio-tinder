36f79e6918f6ab357687939a770ab0dd3be795bf
Faruk Akgul <faakgul@gmail.com>
2016-08-25 13:17:14 +1000

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (setup, find_packages)
 
 setup(
     name="aio-tinder",
-    version="0.1.dev0",
+    version="0.1.dev1",
     description="Tinder API library with asyncio",
     url="https://github.com/faruken/aio-tinder",
     author="faruken",
@@ -20,7 +20,7 @@ setup(
         "Programming Language :: Python :: Implementation :: CPython"
     ],
     keywords="tinder api asyncio",
-    packages=find_packages(exclude=["tests"]),
+    packages=find_packages(exclude=["tests", "examples"]),
     install_requires=[
         "aiohttp==0.22.5",
         "cchardet==1.0.0",
@@ -29,6 +29,7 @@ setup(
         "ujson==1.35"
     ],
     extras_require={
-        "test": ["coverage==4.2", "Logbook==1.0.0", "pytest==3.0.0", "pytest-cov==2.3.1"]
+        "test": ["coverage==4.2", "Logbook==1.0.0", "pytest==3.0.0",
+                 "pytest-asyncio==0.4.1", "pytest-cov==2.3.1"]
     }
 )

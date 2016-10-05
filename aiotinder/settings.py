36e51306cb913a9012d18865a664a2d2c3ed1f28
Faruk Akgul <faruken@users.noreply.github.com>
2016-10-05 20:57:51 +1100

    tinder.app headers

--- aiotinder/settings.py
+++ aiotinder/settings.py
@@ -2,12 +2,12 @@
 
 
 HEADERS = {
-    "User-Agent": "Tinder/6.2.0 (iPhone; iOS 10.0.2; Scale/2.00)",
+    "User-Agent": "Tinder/6.3.0 (iPhone; iOS 10.0.2; Scale/2.00)",
     "Content-Type": "application/json",
     "os_version": "100000000002",
     "platform": "ios",
-    "app-version": "1768",
-    "x-client-version": "62022"
+    "app-version": "1783",
+    "x-client-version": "63009"
 }
 
 API_URL = "https://api.gotinder.com/"

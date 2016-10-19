1c17644ac83a1f9ef12102fafd40993231721bfc
Faruk Akgul <faruken@users.noreply.github.com>
2016-10-20 03:29:53 +1100

    default headers update

--- aiotinder/settings.py
+++ aiotinder/settings.py
@@ -2,12 +2,12 @@
 
 
 HEADERS = {
-    "User-Agent": "Tinder/6.3.0 (iPhone; iOS 10.0.2; Scale/2.00)",
+    "User-Agent": "Tinder/6.3.2 (iPhone; iOS 10.0.2; Scale/2.00)",
     "Content-Type": "application/json",
     "os_version": "100000000002",
     "platform": "ios",
-    "app-version": "1783",
-    "x-client-version": "63009"
+    "app-version": "1786",
+    "x-client-version": "63201"
 }
 
 API_URL = "https://api.gotinder.com/"
